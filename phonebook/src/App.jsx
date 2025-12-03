d8042ffd11539e84c80e0aa0baeaf1d7c0643ed3
Mariofhdez <mario.florez249@gmail.com>
2025-12-03 00:56:14 -0500

    Part2: exercise 2.9

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 function App() {
   const[persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-1234567"}
+    { name: 'Arto Hellas', number: "040-1234567", id: 1},
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -13,6 +17,10 @@ function App() {
 
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value)
+  }
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value)
   }
 
   const addPerson = (e) => {
@@ -33,9 +41,22 @@ function App() {
 
   const exists = (newPerson) => persons.findIndex(p => p.name === newPerson.name)
 
+  let contactsToShow = null;
+
+    if(filter === ""){
+      contactsToShow = persons
+    } else {
+      contactsToShow = persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
+      console.log(contactsToShow);
+    }
+
   return (
     <>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with <input type="text" onChange={handleFilterChange} value={filter} />
+      </div>
+      <h2>Add a new contact</h2>
       <form>
         <div>
           name: <input onChange={handleNameChange} value={newName} type="text" />
@@ -50,8 +71,8 @@ function App() {
       <h2>Contacts</h2>
       <div>
         <ul style={{padding: 0}}>
-          {persons.map(p =>(
-            <li key={p.name} style={{listStyle: 'none'}}><span style={{fontWeight: 'bold'}}>{p.name}</span> {p.number}</li>
+          {contactsToShow.map(p =>(
+            <li key={p.id} style={{listStyle: 'none'}}><span style={{fontWeight: 'bold'}}>{p.name}</span> {p.number}</li>
           ))}
         </ul>
       </div>

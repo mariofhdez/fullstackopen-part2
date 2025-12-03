05dd1a9b83a926f71b42f8b8afa546b768bb9844
Mariofhdez <mario.florez249@gmail.com>
2025-12-03 00:34:09 -0500

    Part2: exercise 2.8

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,18 +2,24 @@ import { useState } from 'react'
 
 function App() {
   const[persons, setPersons] = useState([
-    { name: 'Arturo Hellas'}
+    { name: 'Arto Hellas', number: "040-1234567"}
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
   }
 
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
+  }
+
   const addPerson = (e) => {
     e.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     if(exists(newPerson) !== -1){
@@ -22,6 +28,7 @@ function App() {
 
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   }
 
   const exists = (newPerson) => persons.findIndex(p => p.name === newPerson.name)
@@ -34,6 +41,9 @@ function App() {
           name: <input onChange={handleNameChange} value={newName} type="text" />
         </div>
         <div>
+          number: <input onChange={handleNumberChange} value={newNumber} type="text" />
+        </div>
+        <div>
           <button type='submit' onClick={addPerson} >Add</button>
         </div>
       </form>
@@ -41,7 +51,7 @@ function App() {
       <div>
         <ul style={{padding: 0}}>
           {persons.map(p =>(
-            <li key={p.name} style={{listStyle: 'none'}}>{p.name}</li>
+            <li key={p.name} style={{listStyle: 'none'}}><span style={{fontWeight: 'bold'}}>{p.name}</span> {p.number}</li>
           ))}
         </ul>
       </div>

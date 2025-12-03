df6377b89eacdeae670f251aad308dea60c79249
Mariofhdez <mario.florez249@gmail.com>
2025-12-03 13:20:13 -0500

    Part2: exercise 2.11

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import ContactForm from './components/ContactForm'
 import Contacts from './components/Contacts'
 import Filter from './components/Filter'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-1234567", id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
-  const [contactsToShow, setContactsToShow] = useState(persons)
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response.data);
+        setPersons(response.data)
+        setContactsToShow(response.data)
+      })
+  },[])
+
+  const [contactsToShow, setContactsToShow] = useState([])
 
   const handleFilter = (filter) => {
     if (filter === "") {
